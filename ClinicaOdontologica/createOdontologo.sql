dba0df4179186639dd7404b3e3ac1792af5ec5a3
animolina <anapaulamolinalopez@gmail.com>
2022-06-21 16:23:37 -0300

    Completando clases de acceso a datos: Odontologo Dao H2

---
+++ ClinicaOdontologica/createOdontologo.sql
@@ -0,0 +1,1 @@
+create table IF NOT EXISTS odontologos (id int auto_increment primary key,nombre varchar(255),apellido varchar (255), matricula int);
--- ClinicaOdontologica/src/main/java/com/example/dh/ClinicaOdontologica/Repository/IDaoRepository.java
+++ ClinicaOdontologica/src/main/java/com/example/dh/ClinicaOdontologica/Repository/IDaoRepository.java
@@ -7,7 +7,7 @@ public interface IDaoRepository<T> {
     public T buscarPorId(Long id) throws Exception;
     public List<T> buscarTodos() throws Exception;
     public T actualizar(T t) throws Exception;
-    public Boolean eliminarPorId(Long id) throws Exception;
+    public void eliminarPorId(Long id) throws Exception;
 
     public void eliminarTodos() throws Exception;
 
